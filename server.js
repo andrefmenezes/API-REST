87e2e665406e5323eedb23ad742709420112ce5e
andrefmenezes <decomenezes00@gmail.com>
2021-04-30 15:01:43 -0300

    ok

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const port = process.env.port || 3333
 const routes = require('./src/routes/routes');
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => console.log(`servidor ok `));
+app.listen(port, () => console.log(`servidor ok `));
